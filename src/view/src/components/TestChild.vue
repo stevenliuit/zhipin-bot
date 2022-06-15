7515d1a08c4c6032c02b3d9f9b1914559e675f7e
ksy2019 <gtx_2048@outlook.com>
2022-06-15 20:30:31 +0800

    patch

---
+++ src/view/src/components/TestChild.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>这是子组件{{myname}}</div>
+    <input v-model="myname" type="text">
+</template>
+
+<script>
+export default {
+    props: {
+        myname: String
+    },
+    watch:{
+        myname(){
+            console.log('myname发生变化')
+            this.$emit('update:myname',this.myname)
+        }
+    }
+}
+</script>
+
+<style lang="ts">
+
+</style>
--- src/view/src/views/Index/index.vue
+++ src/view/src/views/Index/index.vue
@@ -1,21 +1,17 @@
 <template>
-  <div>这模板</div>
-  <div @click="ccc($event)" v-if="show"  v-for="item in 3" :[name]="name">这模板2</div>
-  <div v-else :[name]="name">这模板else</div>
-    <h1 v-if="name=='lks'">Vue is awesome!</h1>
-    <h1 v-else-if="name=='one'">hsy</h1>
-    <h1 v-else>Oh no 😢</h1>
-    <input type="checkbox" value="Jack"  v-model="checks"/>
-    
-    <input type="checkbox" value="Jack2"  v-model="checks"/>
-    
-    <input type="checkbox" value="Jack3"  v-model="checks"/>
-
-    <label for="checkbox">{{ checks }}</label>
+    <div>
+        这是父组件{{name}}
+    </div>
+    <TestChild v-model:myname="name"></TestChild>
 </template>
 
 <script>
+import { ref } from "vue";
+import TestChild from '../../components/TestChild.vue'
 export default {
+    components: {
+        TestChild
+    },
     data(){
         return {
             name: 'lks',
